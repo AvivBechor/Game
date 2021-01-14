e5ca2e74107fbecb8ef1c1ec799fa8d8e68a1ab2
AvivBechor <avivbechor@gmail.com>
2021-01-15 00:14:34 +0200

    login
    added a basic login loop and database

--- client.py
+++ client.py
@@ -1,9 +1,13 @@
 import socket
 s=socket.socket()
 s.connect(("127.0.0.1",5555))
+s.send("login".encode())
 while True:
-    s.send("time".encode())
-    time=s.recv(2048)
-    print(time.decode())
-    break
+    data=s.recv(2048)
+    if data.decode()=1:
+        username=input("please insert your username")
+        s.send(username.encode())
+        
+    
+
 
--- server.py
+++ server.py
@@ -2,33 +2,65 @@
 import socket
 import threading
 from datetime import datetime
- 
+import pandas as pd
+import csv
+global usernames
+global passwords
+global conn,addr 
+df= pd.read_csv("usernames and passwords.csv")
+usernames=df["Username"].tolist()
+passwords=df["Password"].tolist()
+usernames.append("test")
 
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ip="0.0.0.0"
 port=5555
-
 s.bind((ip,port))
 s.listen(1)
 print("Waiting for a connection, Server Started")
 conn,addr=s.accept()
 print("Connected to:", addr)
-def exit(conn):
+def exit():
     conn.close()
-def time():
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    return current_time
+def login():
+    #1:get username
+    #2:get password
+    #3:succesful login
+    #01:username not found
+    #02:password not found 
+    conn.send(1.encode())
+    user=conn.revc(2048)
+    while user:
+        if user.decode() in usernames:
+            conn.send(2.encode())
+            pas=conn.revc(2048)
+            while pas:
+                if pas.decode in passwords:
+                    conn.send(3.encode())
+                    break
+                else:
+                    conn.send("02".encode())
+                    pas=conn.revc(2048)
+                    
+           
+        else
+            conn.send("01".encode())
+            user=conn.revc(2048)
+        
+  
 while True:
     data=conn.recv(2048)
+    print(data.decode())
     if data.decode()=="exit":
         exit(conn)
-        break     
+        break
+    eval("{}()".format(data.decode()))
+    '''
     try:
-        result=eval("{}()".format(data.decode()))
-        conn.send(result.encode())
+        eval("{}()".format(data.decode()))
     except:
         conn.send("please enter a valid request".encode())
-        
-    
+ '''       
+
 s.close()
+
