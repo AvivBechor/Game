17540f2bbaafca4abf9df25f42ac6f115201399c
AvivBechor <avivbechor@gmail.com>
2021-01-17 16:19:00 +0200

    correcter the sign up and log in functions
    need to make the sign up a locked function so no 2 connection could write into the database at the same time

--- server.py
+++ server.py
@@ -5,48 +5,12 @@ from datetime import datetime
 import pandas as pd
 import csv
 import time
-global usernames
-global passwords
-global conn,addr 
-df= pd.read_csv("usernames and passwords.csv")
-usernames=df["Username"].tolist()
-passwords=df["Password"].tolist()
-s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ip="0.0.0.0"
-port=5555
-s.bind((ip,port))
-s.listen(1)
-print("Waiting for a connection, Server Started")
-conn,addr=s.accept()
-print("Connected to:", addr)
+global conn,addr
+global df
+df=pd.read_csv("usernames and passwords.csv")
+
 def exit():
     conn.close()
-def signup():#this is NOT good, this is just temporary. To do: instead of 2 lists work straight on the df and update it each time u make a new user.
-    #1.1:get username-signup
-    #2.1:get password-signup
-    #3.1:signup succesful
-    #01.1:username already exists
-    #02.1:password already exists 
-    conn.send(b'1.1')
-    user=conn.recv(2048)
-    while user:
-        if user.decode() in usernames:
-            conn.send(b'01.1')
-            user=conn.recv(2048)
-        else:
-            conn.send(b'2.1')
-            pas=conn.recv(2048)
-            while pas:
-                if pas in passwords:
-                    conn.send(b'02.1')
-                    user=conn.recv(2048)
-                else:
-                    uasernames.append(user)
-                    passwords.append(pas)
-                    new=(usernames[-1],passwords[-1])
-                    df.append(new)
-                    
-            
 def login():
     #1:get username
     #2:get password
@@ -56,13 +20,12 @@ def login():
     conn.send(b'1')
     user=conn.recv(2048)
     while user:
-        if user.decode() in usernames:
-            i=usernames.index(user.decode())
-            print(usernames[i])
+        if user.decode() in list(df["Username"]):
+            i=list(df["Username"]).index(user.decode())
             conn.send(b'2')
             pas=conn.recv(2048)
             while pas:
-                if passwords[i]==pas.decode():
+                if list(df["Password"])[i]==pas.decode():
                     conn.send(b'3')
                     user=None
                     break
@@ -75,8 +38,47 @@ def login():
             conn.send("01".encode())
             user=conn.recv(2048)
             continue
-    
-  
+def signup():#make it a locked action so 2 connections couldnt write to the database at the same time 
+    #1.1:get username-signup
+    #2.1:get password-signup
+    #3.1:signup succesful
+    #01.1:username already exists
+    #02.1:password already exists
+    df=pd.read_csv("usernames and passwords.csv")
+    conn.send(b'1.1')
+    user=conn.recv(2048)
+    while user:
+        if user.decode() in list(df["Username"]):
+            conn.send(b'01.1')
+            user=conn.recv(2048)
+        else:
+            conn.send(b'2.1')
+            pas=conn.recv(2048)
+            while pas:
+                if pas.decode() in list(df["Password"]):
+                    conn.send(b'02.1')
+                    pas=conn.recv(2048)
+                else:
+                    s=pd.Series({"Username":user.decode(),"Password":pas.decode()})
+                    df=df.append(s,ignore_index=True)
+                    df.to_csv("usernames and passwords.csv",index=False)
+                    conn.send(b'3.1')
+                    pas=None
+                    user=None
+                    print(df)
+                    break
+                    
+#server
+s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ip="0.0.0.0"
+port=5555
+s.bind((ip,port))
+s.listen(1)
+print("Waiting for a connection, Server Started")
+conn,addr=s.accept()
+print("Connected to:", addr)
+
+
 while True:
     data=conn.recv(2048)
     print(data.decode())
