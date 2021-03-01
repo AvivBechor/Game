5f9c5b0b17c9e830532c2154ad30a5a476b445ae
FarmingFrenzy <46293740+FarmingFrenzy@users.noreply.github.com>
2021-03-01 18:00:35 +0200

    Rewrote attack script, player movement script, and added a player animator controller script
    title, also added comments

---
+++ 8-bit Dungeon - INternet Explorers/Assets/Scripts/PlayerAnimatorController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts;
+
+public class PlayerAnimatorController : MonoBehaviour
+{
+    private Animator animator;
+    private Player player;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        player = GetComponent<Player>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("moving", player.isMoving);
+        switch (player.rotation)
+        {
+            case Side.UP:
+                animator.SetFloat("moveY", 1);
+                animator.SetFloat("moveX", 0);
+                break;
+            case Side.DOWN:
+                animator.SetFloat("moveY", -1);
+                animator.SetFloat("moveX", 0);
+                break;
+            case Side.LEFT:
+                animator.SetFloat("moveX", -1);
+                animator.SetFloat("moveY", 0);
+                break;
+            case Side.RIGHT:
+                animator.SetFloat("moveX", 1);
+                animator.SetFloat("moveY", 0);
+                break;
+        }
+    }
+}
