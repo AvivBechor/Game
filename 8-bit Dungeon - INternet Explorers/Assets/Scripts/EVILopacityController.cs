fdac8de2ca697fcb3d5ff6fa62849999bdceb14f
AvivBechor <avivbechor@gmail.com>
2021-05-10 19:16:23 +0300

    forest

---
+++ 8-bit Dungeon - INternet Explorers/Assets/Scripts/EVILopacityController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EVILopacityController : MonoBehaviour
+{
+    public Image opacityPanelImage;
+    public LayerMask evilGround;
+
+    void Start()
+    {
+        opacityPanelImage = GameObject.Find("EVILpanel").GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics2D.OverlapCircle(transform.position, .2f, evilGround))
+        {
+            Debug.Log("ELI");
+            if (opacityPanelImage.color.a < 0.4)
+                opacityPanelImage.color = new Color(opacityPanelImage.color.r, opacityPanelImage.color.g, opacityPanelImage.color.b, opacityPanelImage.color.a + 0.05f);
+        }
+        else
+        {
+            if (opacityPanelImage.color.a > 0)
+                opacityPanelImage.color = new Color(opacityPanelImage.color.r, opacityPanelImage.color.g, opacityPanelImage.color.b, opacityPanelImage.color.a - 0.05f);
+        }
+    }
+}
